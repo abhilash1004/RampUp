44be465fdd86b52611ff09bad46b0e6825227b83
Pratyusha Perla <abhilash.perla10@gmail.com>
2022-04-28 18:23:55 +0530

    Adding basic templates of client and server ends

---
+++ RampUp_server/src/test/java/com/rampup/RampUpServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rampup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RampUpServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
