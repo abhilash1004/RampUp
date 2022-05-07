9d35744f648495e405e39afe6f74015327ed45ab
abhilash1004 <58764957+abhilash1004@users.noreply.github.com>
2022-05-08 00:38:48 +0530

    Fixed linter errors in signincontroller

--- RampUp_server/src/main/java/com/rampup/controller/SignInController.java
+++ RampUp_server/src/main/java/com/rampup/controller/SignInController.java
@@ -2,7 +2,7 @@ package com.rampup.controller;
 
 import java.util.Optional;
 
-import org.json.simple.JSONObject;    
+import org.json.simple.JSONObject;   
 import org.json.simple.JSONValue;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,12 +20,12 @@ import com.rampup.repo.UsersRepository;
 
 @RestController
 @CrossOrigin(origins = "*")
-public class SignInController{
+public class SignInController {
 	@Autowired
 	private UsersRepository userrepo;
 	
 	// APi Example: " http://localhost:8080/getUsers "
-	@GetMapping(path="/getUsers", produces = "application/json" )
+	@GetMapping(path = "/getUsers", produces = "application/json")
 	public ResponseEntity<?> getUsers(){
 		return ResponseEntity.ok(this.userrepo.findAll());
 	}
@@ -33,38 +33,42 @@ public class SignInController{
 	
 	// Api Example " http://localhost:8080/login?username=abc@gmail.com&password=123456 "
 	@GetMapping("/login")
-	public String login(@RequestParam("username") String username, @RequestParam("password") String password) {
+	public String login(@RequestParam("username") final String username, 
+			    @RequestParam("password") final String password) {
 		Optional<User> opuser = userrepo.findById(username);
-		if(!opuser.isPresent()) {
+		if (!opuser.isPresent()) {
 			return "No User Exists";
 		}
 		User user = userrepo.findById(username).orElse(null);
-		if(user == null) {
+		if (user == null) {
 			throw new NullPointerException();
 		}
-		if(user.getUsername().equals(username) && user.getPassword().equals(password)) {
+		if(user.getUsername().equals(username) &&
+		   user.getPassword().equals(password)) {
 			return "Successfull";
 		}
 		return "Invalid Credentials";
 	}
 	
-	//  Api Example " http://localhost:8080/login " [And in body both username and password fields should be present]
+	//  Api Example " http://localhost:8080/login "
+	//  [And in body both username and password fields should be present]
 	@PostMapping("/login")
-	public String login(@RequestBody String jsonBody) {
+	public String login(@RequestBody final String jsonBody) {
 		Object obj = JSONValue.parse(jsonBody);
 		JSONObject jsonObject = (JSONObject) obj;
-		String username = (String)jsonObject.get("username"); 
-		String password = (String)jsonObject.get("password");
+		String username = (String) jsonObject.get("username");
+		String password = (String) jsonObject.get("password");
 		System.out.println(username + " " + password + " are values");
 		Optional<User> opuser = userrepo.findById(username);
-		if(!opuser.isPresent()) {
+		if (!opuser.isPresent()) {
 			return "No User Exists";
 		}
 		User user = userrepo.findById(username).orElse(null);
-		if(user == null) {
+		if (user == null) {
 			throw new NullPointerException();
 		}
-		if(user.getUsername().equals(username) && user.getPassword().equals(password)) {
+		if (user.getUsername().equals(username) &&
+		    user.getPassword().equals(password)) {
 			return "Successfull";
 		}
 		return "Invalid Credentials";
